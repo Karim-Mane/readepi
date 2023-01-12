70a1752800f39df5d4ac6009c2b0ba00a20dfba9
Karim Mane <karimmane@iMAC-HOTDESKUSER.local>
2023-01-12 15:33:49 +0000

    update initial reading function

--- R/read_stuff.R
+++ R/read_stuff.R
@@ -11,16 +11,9 @@
 #' @examples
 #' some_path <- "path to your file"
 #' read_stuff(some_path)
-
 read_stuff <- function(x) {
   # check inputs
   checkmate::assertCharacter(x, len = 1L)
-
-<<<<<<< HEAD
-
-=======
-  
->>>>>>> origin/main
   # do stuff
   x
 }
@@ -29,38 +22,31 @@ read_stuff <- function(x) {
 
 
 
-
-<<<<<<< HEAD
 #' function to read data from all supported file types in the `rio` package.
 #' @param file.path the path to the file to be read. When several files need to be imported from a directory, this should be the path to that directory
 #' @param sep the separator between the columns in the file. This is only required for space-separated files.
-=======
-#' function to read data from all supported file types in the `rio` package. 
+#' function to read data from all supported file types in the `rio` package.
 #' @param file.path the path to the file to be read. When several files need to be imported from a directory, this should be the path to that directory
-#' @param sep the separator between the columns in the file. This is only required for space-separated files. 
->>>>>>> origin/main
+#' @param sep the separator between the columns in the file. This is only required for space-separated files.
 #' @param format a string used to specify the file format. This is useful when a file does not have an extension, or has a file extension that does not match its actual type
 #' @param which a string used to specify the name of the excel sheet to import
 #' @param pattern when specified, only files with this suffix will be imported from the specified directory
 #' @returns  a data frame (import from file) or a list (import several files from directory)
 #' @examples data = read_from_file(file.path = "/path/to/test.txt", sep=NULL, format=NULL, which=NULL, pattern=NULL)
-<<<<<<< HEAD
 #'
+# read_from_file = function(file.path, sep=NULL, format=NULL, which=NULL, pattern=NULL){
+#   # check if the file exists
+#   if(!file.exists(file.path) & !dir.exists(file.path)){
+#     stop(file.path, " No such file or directory!")
+#   }
+
+
 read_from_file = function(file.path, sep=NULL, format=NULL, which=NULL, pattern=NULL){
   # check if the file exists
   if(!file.exists(file.path) & !dir.exists(file.path)){
     stop(file.path, " No such file or directory!")
   }
 
-=======
-#' @export
-read_from_file = function(file.path, sep=NULL, format=NULL, which=NULL, pattern=NULL){
-  # check if the file exists
-  if(!file.exists(file.path) & !dir.exists(file.path)){ 
-    stop(file.path, " No such file or directory!")
-  }
-  
->>>>>>> origin/main
   # reading data from file
   if(file.exists(file.path) & !dir.exists(file.path)){
     if(is.null(sep)){
@@ -72,11 +58,7 @@ read_from_file = function(file.path, sep=NULL, format=NULL, which=NULL, pattern=
       data = data.table::fread(file.path, sep = sep)
     }
   }
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> origin/main
   # reading several files from a directory
   if(dir.exists(file.path) & length(list.files(file.path))>0){
     if(!is.null(pattern)) data = rio::import_list(list.files(file.path, full.names = TRUE, pattern = pattern))
@@ -87,17 +69,14 @@ read_from_file = function(file.path, sep=NULL, format=NULL, which=NULL, pattern=
 
 # read_from_file = function(file.path, sep=NULL, sheet.name=NULL){
 #   # check if the file exists
-<<<<<<< HEAD
 #   if(!file.exists(file.path)){
 #     stop(file.path, " not found!")
 #   }
 #
-=======
-#   if(!file.exists(file.path)){ 
+#   if(!file.exists(file.path)){
 #     stop(file.path, " not found!")
 #   }
-#   
->>>>>>> origin/main
+#
 #   # reading the file
 #   file.extension = getExtension(file.path)
 #   if(file.extension %in% c("xlsx","xls")){
@@ -106,11 +85,6 @@ read_from_file = function(file.path, sep=NULL, format=NULL, which=NULL, pattern=
 #   }else if(file.extension %in% c("json","JSON")){
 #     data = jsonlite::fromJSON(file.path, simplifyDataFrame=TRUE)
 #   }else if(file.extension %in% c("xml","XML")){
-<<<<<<< HEAD
-#
-=======
-#     
->>>>>>> origin/main
 #   }else{
 #     if(!is.null(sep)) data = fread(file.path, sep = sep, nThread = 4)
 #     else data = fread(file.path, nThread = 4)
@@ -133,7 +107,6 @@ getExtension = function(file.path){
 #' @param database.name the name of the database that contains the table from which the data should be pulled
 #' @param table.name the name of the target table
 #' @param driver.name the name of the MS driver. use `odbc::odbcListDrivers()` to display the list of installed drivers
-<<<<<<< HEAD
 #' @param records a vector or a comma-separated string of subset of subject IDs. When specified, only the records that correspond to these subjects will be imported.
 #' @param fields a vector or a comma-separated string of column names. If provided, only those columns will be imported
 #' @param id.position the column position of the variable that unique identifies the subjects. This should only be specified when the column with the subject IDs is not the first column. default is 1.
@@ -144,18 +117,6 @@ read_from_ms_sql_server = function(user, password, host, port, database.name, dr
   # reading in user credentials
   # credentials = readMSsqlCredentials(credentials.file)
 
-=======
-#' @param records a vector or a comma-separated string of subset of subject IDs. When specified, only the records that correspond to these subjects will be imported.   
-#' @param fields a vector or a comma-separated string of column names. If provided, only those columns will be imported
-#' @param id.position the column position of the variable that unique identifies the subjects. This should only be specified when the column with the subject IDs is not the first column. default is 1.  
-#' @returns a data frame
-#' @export
-#' @examples data = read_from_relationalDB(user="kmane", password="****", host="robin.mrc.gm", port=1433, database.name="mrcg_db", table.name="test", driver.name="ODBC Driver 17 for SQL Server")
-read_from_ms_sql_server = function(user, password, host, port, database.name, driver.name, table.name, records=NULL, fields=NULL, id.position=1){
-  # reading in user credentials
-  # credentials = readMSsqlCredentials(credentials.file)
-  
->>>>>>> origin/main
   # establishing the connection to the server
   con = DBI::dbConnect(odbc::odbc(),
                        driver = driver.name,
@@ -164,41 +125,29 @@ read_from_ms_sql_server = function(user, password, host, port, database.name, dr
                        uid = user,
                        pwd = password,
                        port = as.numeric(port))
-<<<<<<< HEAD
 
   # listing the names of the tables present in the database
   tables = DBI::dbListTables(conn = con)
 
-=======
-  
   # listing the names of the tables present in the database
   tables = DBI::dbListTables(conn = con)
-  
->>>>>>> origin/main
+
   # checking if the specified table exists in the database
   if(!(table.name %in% tables)){
     stop("Could not found table called ",table.name," in ",database.name,"!\n")
   }
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> origin/main
   # extract the data from the given table and store the output in an R object
   sql = DBI::dbSendQuery(con,paste0("select * from ",table.name))
   data = DBI::dbFetch(sql, -1)
   DBI::dbClearResult(sql)
-<<<<<<< HEAD
 
   # closing the connection
   DBI::dbDisconnect(conn = con)
 
-=======
-  
   # closing the connection
   DBI::dbDisconnect(conn = con)
-  
->>>>>>> origin/main
+
   # subsetting
   if(!is.null(fields)){
     if(is.character(fields)){
@@ -222,11 +171,6 @@ read_from_ms_sql_server = function(user, password, host, port, database.name, dr
     }
     data = data %>% filter(data[[id.column.name]] %in% records)
   }
-<<<<<<< HEAD
-
-=======
-  
->>>>>>> origin/main
   data
 }
 
@@ -235,11 +179,7 @@ readMSsqlCredentials = function(credentials.file){
   if(!file.exists(credentials.file)){
     stop("Could not find ",credentials.file)
   }
-<<<<<<< HEAD
   credentials = data.table::fread(credentials.file,
-=======
-  credentials = data.table::fread(credentials.file, 
->>>>>>> origin/main
                                   sep = "\t")
   if(ncol(credentials)!=4){
     stop("credential file should be tab-separated file with 4 columns.")
@@ -254,7 +194,6 @@ readMSsqlCredentials = function(credentials.file){
 }
 
 #' function to read data from REDCap
-<<<<<<< HEAD
 #' @param uri the URI of the server
 #' @param token the user-specific string that serves as the password for a project
 #' @param project.id the name of the project you want to import data from
@@ -263,28 +202,12 @@ readMSsqlCredentials = function(credentials.file){
 #' @param fields a vector or a comma-separated string of column names. If provided, only those columns will be imported.
 #' @examples redcap.data = read_from_redcap(uri, token, project.id, id_position=1, records=NULL, fields=NULL)
 #' @returns a list with 2 data frames: the data of interest and the metadata associated to the data.
-#'
-=======
-#' @param uri the URI of the server   
-#' @param token the user-specific string that serves as the password for a project
-#' @param project.id the name of the project you want to import data from
-#' @param id.position the column position of the variable that unique identifies the subjects. This should only be specified when the column with the subject IDs is not the first column. default is 1.   
-#' @param records a vector or a comma-separated string of subset of subject IDs. When specified, only the records that correspond to these subjects will be imported.   
-#' @param fields a vector or a comma-separated string of column names. If provided, only those columns will be imported.   
-#' @examples redcap.data = read_from_redcap(uri, token, project.id, id_position=1, records=NULL, fields=NULL)
-#' @returns a list with 2 data frames: the data of interest and the metadata associated to the data.      
-#' @export
->>>>>>> origin/main
 read_from_redcap = function(uri, token, project.id, id.position=1L, records=NULL, fields=NULL){
   # importing the credentials
   # credentials = readCredentials(credentials.file, project.id)
   # redcap.uri = uri
   # token = token
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> origin/main
   # importing data and the metadata into R
   if(is.null(records) & is.null(fields)){
     # redcap.data = REDCapR::redcap_read_oneshot(redcap_uri = redcap.uri, token = token, records=NULL, fields=NULL, verbose = FALSE)
@@ -317,21 +240,13 @@ read_from_redcap = function(uri, token, project.id, id.position=1L, records=NULL
     redcap.data = REDCapR::redcap_read(redcap_uri = uri, token = token, id_position=as.integer(id.position), records_collapsed=records, verbose = FALSE)
     metadata = REDCapR::redcap_metadata_read(redcap_uri = uri, token = token, verbose = FALSE)
   }
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> origin/main
   # checking whether the importing was successful and extract the desired records and columns
   if(redcap.data$success & metadata$success){
-    data = as.data.table(redcap.data$data)
-    meta = as.data.table(metadata$data)
+    data = redcap.data$data
+    meta = metadata$data
   }
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> origin/main
   # return the imported data
   list(data = data,
        metadata = meta)
@@ -343,12 +258,7 @@ readCredentials = function(file.path, project.id){
   if(!file.exists(file.path)){
     stop("Could not find ",file.path)
   }
-<<<<<<< HEAD
-  credentials = data.table::fread(file.path,
-=======
-  credentials = data.table::fread(file.path, 
->>>>>>> origin/main
-                                  sep = "\t")
+  credentials = data.table::fread(file.path, sep = "\t")
   if(ncol(credentials)!=7){
     stop("credential file should be tab-separated file with 7 columns.")
   }
@@ -375,44 +285,33 @@ readCredentials = function(file.path, project.id){
 
 #' Function to import epidemiology-related data from different sources into R
 #' @description the function is a wrapper around numerous functions that are used to import data stored in database management systems (DBMS) including both relational and NoSQL databases. It also contains functions to import data from common file types such as csv, txt, xlsx, xml, json, etc.
-<<<<<<< HEAD
 #' @param credentials.file the path to the file with the user-specific credential details for the projects of interest. It is required when importing data from DBMS. This is a tab-delimited file with the following columns:
-=======
-#' @param credentials.file the path to the file with the user-specific credential details for the projects of interest. It is required when importing data from DBMS. This is a tab-delimited file with the following columns:   
->>>>>>> origin/main
+#' @param credentials.file the path to the file with the user-specific credential details for the projects of interest. It is required when importing data from DBMS. This is a tab-delimited file with the following columns:
 #' \enumerate{
 #'   \item user_name: the user name
 #'   \item password: the user password (for REDCap, this corresponds to the **token** that serves as password to the project)
 #'   \item host_name: the host name (for relational DB) or the URI (for NoSQL DB such as REDCap)
-<<<<<<< HEAD
 #'   \item project_id: the project ID or the name of the database you are access to.
-=======
-#'   \item project_id: the project ID or the name of the database you are access to. 
->>>>>>> origin/main
+#'   \item project_id: the project ID or the name of the database you are access to.
 #'   \item comment: a summary description about the project or database of interest
 #'   \item dbms: the name of the DBMS: 'redcap' or 'REDCap' when reading from REDCap, 'sqlserver' or 'SQLServer' when reading from MS SQLServer
 #'   \item port: the port ID (used for MS SQLServers)
 #'   }
 #' @param file.path the path to the file to be read. When several files need to be imported from a directory, this should be the path to that directory
-<<<<<<< HEAD
 #' @param sep the separator between the columns in the file. This is only required for space-separated files.
-=======
-#' @param sep the separator between the columns in the file. This is only required for space-separated files. 
->>>>>>> origin/main
+#' @param sep the separator between the columns in the file. This is only required for space-separated files.
 #' @param format a string used to specify the file format. This is useful when a file does not have an extension, or has a file extension that does not match its actual type
 #' @param which a string used to specify which objects should be extracted (the name of the excel sheet to import)
 #' @param pattern when specified, only files with this suffix will be imported from the specified directory
 #' @param project.id for relational DB, this is the name of the database that contains the table from which the data should be pulled. Otherwise, it is the project ID you were given access to.
 #' @param driver.name the name of the MS driver. use `odbc::odbcListDrivers()` to display the list of installed drivers
 #' @param table.name the name of the target table
-<<<<<<< HEAD
 #' @param records a vector or a comma-separated string of subset of subject IDs. When specified, only the records that correspond to these subjects will be imported.
 #' @param fields a vector or a comma-separated string of column names. If provided, only those columns will be imported.
 #' @param id.position the column position of the variable that unique identifies the subjects. This should only be specified when the column with the subject IDs is not the first column. default is 1.
 #' @example data = readepi(credentials.file, project.id="test")
 #' @returns a list with 2 data frames (data and metadata) when reading from REDCap. A data frame otherwise.
 #' @export
-#'
 readepi = function(credentials.file=NULL,
                     file.path=NULL,
                     sep=NULL,
@@ -425,75 +324,32 @@ readepi = function(credentials.file=NULL,
                     records=NULL,
                     fields=NULL,
                     id.position=1){
-
-=======
-#' @param records a vector or a comma-separated string of subset of subject IDs. When specified, only the records that correspond to these subjects will be imported.   
-#' @param fields a vector or a comma-separated string of column names. If provided, only those columns will be imported.   
-#' @param id.position the column position of the variable that unique identifies the subjects. This should only be specified when the column with the subject IDs is not the first column. default is 1.  
-#' @example data = readepi(credentials.file, project.id="test")
-#' @returns a list with 2 data frames (data and metadata) when reading from REDCap. A data frame otherwise.   
-#' @export
-#' 
-readepi = function(credentials.file=NULL, 
-                    file.path=NULL, 
-                    sep=NULL, 
-                    format=NULL, 
-                    which=NULL,
-                    pattern=NULL,
-                    project.id = NULL,
-                    driver.name=NULL, 
-                    table.name=NULL,
-                    records=NULL, 
-                    fields=NULL,
-                    id.position=1){
-  
->>>>>>> origin/main
   # some check points
   if(!is.null(credentials.file) & !is.null(file.path)){
     stop("Impossible to import data from DBMS and file at the same time.")
   }
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> origin/main
   # reading from file
   if(!is.null(file.path)){
     res = read_from_file(file.path, sep=sep, format=format, which=which, pattern=pattern)
   }
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> origin/main
   # reading from DBMS
   if(!is.null(credentials.file)){
     credentials = readCredentials(credentials.file, project.id)
     if(credentials$dbms %in% c('redcap','REDCap')){
-<<<<<<< HEAD
       res = read_from_redcap(uri=credentials$host, token=credentials$pwd, project.id=credentials$project, id.position=id.position,
                              records=records, fields=fields)
     }else if(credentials$dbms %in% c('sqlserver','SQLServer')){
       res = read_from_ms_sql_server(user=credentials$user, password=credentials$pwd, host=credentials$host, port=credentials$port,
-=======
-      res = read_from_redcap(uri=credentials$host, token=credentials$pwd, project.id=credentials$project, id.position=id.position, 
-                             records=records, fields=fields)
-    }else if(credentials$dbms %in% c('sqlserver','SQLServer')){
-      res = read_from_ms_sql_server(user=credentials$user, password=credentials$pwd, host=credentials$host, port=credentials$port, 
->>>>>>> origin/main
                                     database.name=credentials$project, table.name=table.name, driver.name=driver.name, records=records,
                                     fields=fields)
     }
   }
-<<<<<<< HEAD
 
   res
 }
-=======
-  
-  res
-} 
->>>>>>> origin/main
+
 
 
 
